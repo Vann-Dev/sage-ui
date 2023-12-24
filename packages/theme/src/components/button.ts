f75f6fd850e1170fbe17ca9ac884bfacad13b7e2
Vann-Dev <stevanvincent05@gmail.com>
2023-12-24 11:11:50 +0700

    feat(theme): more color

--- packages/theme/src/components/button.ts
+++ packages/theme/src/components/button.ts
@@ -1,16 +1,21 @@
 import { VariantProps, tv } from "tailwind-variants";
 
 const button = tv({
-    base: "font-medium px-6 py-3 rounded-lg hover:opacity-80",
+    base: "font-medium px-6 py-3 rounded-lg enabled:hover:opacity-80 transition-opacity duration-100 disabled:opacity-80",
     variants: {
         color: {
-            primary: "bg-green-500",
-            secondary: "border-green-500 border-2 text-white"
+            primary: "bg-[#4FB055]",
+            secondary: "bg-zinc-600",
+            outline: "border-[#4FB055] border-2 text-white",
+            danger: "bg-red-700",
+            warning: "bg-yellow-600",
+            ghost: "bg-transparent",
+            link: "hover:underline"
         },
         size: {
             sm: "text-sm",
             md: "text-base",
-            lg: "text-lg"
+            lg: "text-lg px-8 py-4"
         }
     },
     defaultVariants: {
